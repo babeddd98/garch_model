7e8f6ead046b11ca9bc828eac18184eff708f9f3
babeddd98 <83009986+babeddd98@users.noreply.github.com>
2023-01-31 21:16:42 +0100

    Add filling bewteen curves on VaR plot, add GARCH estimation plot

--- VaR_modeling.py
+++ VaR_modeling.py
@@ -1,50 +1,46 @@
 import yfinance as yf
-import matplotlib.pyplot as plt
-from datetime import datetime
 from arch import arch_model
-import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from datetime import datetime
 
-ticker1 = "^FCHI" # CAC40
-
-data = yf.download(ticker1, 
-                   start=datetime(2010,1,1), 
-                   end=datetime(2023,1,1))
-
-data["Returns"] = 100 * (data["Close"].pct_change())
-x = data["Returns"].dropna()
-
-
-am = arch_model(x , p = 1, q = 1, mean='constant', vol = 'EGARCH', dist = 't')
-res = am.fit(update_freq=10)
-
-forecasts = res.forecast(start='2019-01-01')
-cond_mean = forecasts.mean['2019':]
-cond_var = forecasts.variance['2019':]
-q = am.distribution.ppf([0.01, 0.05], res.params[4])
-
-value_at_risk = -cond_mean.values - np.sqrt(cond_var).values * q[None, :]
-value_at_risk = pd.DataFrame(value_at_risk, columns=['1%', '5%'], index=cond_var.index)
-
-ax = value_at_risk.plot(legend=False, figsize=(12,6))
-xl = ax.set_xlim(value_at_risk.index[0], value_at_risk.index[-1])
-
-rets_2019 = x['2019':]
+def garch_model(x, p, q, vol="GARCH"):
+    garch = arch_model(x, p, q, mean="constant", vol=vol, dist="normal")
+    garch_fit = garch.fit(update_freq=10)
+    return garch_fit
 
 
-c = []
-for idx in value_at_risk.index:
-    if rets_2019[idx] > -value_at_risk.loc[idx, '5%']:
-        c.append('#000000')
-    elif rets_2019[idx] < -value_at_risk.loc[idx, '1%']:
-        c.append('#BB0000')
+ticker = "^FCHI"
+data = yf.download(ticker, start=datetime(2010,1,1), end=datetime(2023,1,1))
+data["Returns"] = 100*data["Close"].pct_change()
+data = data.dropna()
+
+garch = arch_model(data["Returns"], p=1, q=1, mean="constant", vol="EGARCH", dist="t")
+garch_fit = garch.fit(update_freq=10)
+
+
+f = garch_fit.forecast(start="2022-01-01")
+mean_f = f.mean["2022":]
+variance_f = f.variance["2022":]
+quartile = garch.distribution.ppf([0.01, 0.05], garch_fit.params[4])
+value_at_risk = -mean_f.values - np.sqrt(variance_f).values * quartile
+value_at_risk = pd.DataFrame(value_at_risk, columns=['1%', '5%'], index=variance_f.index)
+dates_1year = data.Returns["2022":]
+
+colors = []
+for i in value_at_risk.index:
+    if dates_1year[i] > -value_at_risk.loc[i, "5%"]:
+        colors.append("#000000")
+    elif dates_1year[i] < -value_at_risk.loc[i, "1%"]:
+        colors.append("#BB0000")
     else:
-        c.append('#BB00BB')
-        
-c = np.array(c, dtype='object')
+        colors.append("#BB00BB")
+
+colors = np.array(colors)
 
 labels = {
-    
+
     '#BB0000': '1% Exceedence',
     '#BB00BB': '5% Exceedence',
     '#000000': 'No Exceedence'
@@ -52,16 +48,27 @@ labels = {
 
 markers = {'#BB0000': 'x', '#BB00BB': 's', '#000000': 'o'}
 
-for color in np.unique(c):
-    sel = c == color
-    ax.scatter(
-        rets_2019.index[sel],
-        -rets_2019.loc[sel],
+figure, [ax, value_at_risk_plot] = plt.subplots(2, 1)
+
+value_at_risk_plot.plot(value_at_risk)
+
+for color in np.unique(colors):
+    sel = colors == color
+    value_at_risk_plot.scatter(
+        dates_1year.index[sel],
+        -dates_1year.loc[sel],
         marker=markers[color],
-        c=c[sel],
+        c=colors[sel],
         label=labels[color])
-    
-ax.set_title('Parametric VaR')
-ax.legend(frameon=False, ncol=3)
-ax.fill_between(value_at_risk.index, value_at_risk['1%'], value_at_risk['5%'], color="red", alpha=0.2)
+
+value_at_risk_plot.set_xlim([value_at_risk.index[0], value_at_risk.index[-1]])
+value_at_risk_plot.set_title('Parametric value_at_risk')
+value_at_risk_plot.legend()
+value_at_risk_plot.fill_between(value_at_risk.index, value_at_risk['1%'], value_at_risk['5%'], color="red", alpha=0.2)
+
+ax.plot(abs(data["Returns"]["2022":]), label="realised volatility", color="grey")
+ax.plot(garch_fit.conditional_volatility, label="estimated volatility", color="red")
+ax.set_xlim([value_at_risk.index[0], value_at_risk.index[-1]])
+ax.set_title("CAC40 volatility estimation with GARCH model")
+ax.legend()
 plt.show()
